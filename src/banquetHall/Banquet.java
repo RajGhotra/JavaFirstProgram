64585e1491a6ddb1c2973a9be33bda3c09a658f9
unknown <kaurrajinder95@gmail.com>
2024-04-23 18:22:20 -0400

    changes in assignment

--- src/banquetHall/Banquet.java
+++ src/banquetHall/Banquet.java
@@ -1,18 +1,13 @@
 package banquetHall;
 
 public class Banquet {
-	
-	double totalBookingBaseCost;
+
 	double bookingBaseCost;
 	double foodCost;
 	double beverageCost;
 	double taxOnFood;
 	double taxOnBeverages;
 	double tip;
-     int  noOfGuests;
-     double cessTax;
-     double totalCost;
-     double taxApplied;
 
 	public Banquet(double bookingBaseCost, double foodCost, double beverageCost, double tip, double taxOnFood,
 			double taxOnBeverages) {
@@ -25,36 +20,38 @@ public class Banquet {
 		this.taxOnFood = taxOnFood;
 	}
 
-	public double calculateBaseCost(double bookingBaseCost ,double foodCost, double beverageCost, double tip) {
+	public double calculateBaseCost(double bookingBaseCost, double foodCost, double beverageCost) {
 
-	
-		return totalBookingBaseCost =  bookingBaseCost+foodCost + beverageCost + tip;
-
+		double totalBookingBaseCost = bookingBaseCost + foodCost + beverageCost;
+		return totalBookingBaseCost;
 	}
 
 	public double calculateTax(double bookingBaseCost) {
-      
-		
-		return  taxApplied=bookingBaseCost*(taxOnFood/100)+(taxOnBeverages/100);
+
+		double taxApplied = bookingBaseCost*((taxOnFood / 100) + (taxOnBeverages / 100));
+		return taxApplied;
 
 	}
- public double calculateCess(int  noOfGuests, double totalBookingBaseCost) {
-	 if(noOfGuests<=40) {
-		 
-		 cessTax=totalBookingBaseCost*4/100;
-		
-	 }else if(noOfGuests>40 && noOfGuests<=80) {
-		 cessTax=totalBookingBaseCost*8/100;
-	 }else if(noOfGuests>80 && noOfGuests<=150) {
-		 cessTax=totalBookingBaseCost*10/100;
-	 }else if(noOfGuests>150) {
-		 cessTax=totalBookingBaseCost*12.5/100;
-	 }
-	return cessTax;
-	 
-	 
- }
- public double calculateTheTotalCost( double totalBookingBaseCost,double taxAppied,double cessTax ) {
-	 return totalCost = totalBookingBaseCost+taxAppied+cessTax;
- }
+
+	public double calculateCess(int noOfGuests, double totalBookingBaseCost) {
+		double cessTax = 0;
+		if (noOfGuests <= 40) {
+
+			cessTax = totalBookingBaseCost * 4 / 100;
+
+		} else if (noOfGuests > 40 && noOfGuests <= 80) {
+			cessTax = totalBookingBaseCost * 8 / 100;
+		} else if (noOfGuests > 80 && noOfGuests <= 150) {
+			cessTax = totalBookingBaseCost * 10 / 100;
+		} else if (noOfGuests > 150) {
+			cessTax = totalBookingBaseCost * 12.5 / 100;
+		}
+		return cessTax;
+
+	}
+
+	public double calculateTheTotalCost(double totalBookingBaseCost, double taxAppied, double cessTax) {
+		double totalCost = totalBookingBaseCost + taxAppied + cessTax + tip;
+		return totalCost;
+	}
 }
