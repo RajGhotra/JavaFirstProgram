79d34692a47ba9867bba06ebd533721376828f86
unknown <kaurrajinder95@gmail.com>
2024-04-23 18:18:30 -0400

    Changes in banquet Hall

--- src/banquetHall/MainBanquet.java
+++ src/banquetHall/MainBanquet.java
@@ -22,12 +22,20 @@ public class MainBanquet {
 		double tip1 = sc.nextDouble();
 		System.out.println("Enter the number of guests");
 		int numguests = sc.nextInt();
-
+        if(costOfBooking>0&&costOfFood>0&&beveragesCost>0) {
+	
+	
 	Banquet hall=new Banquet(costOfBooking, costOfFood, beveragesCost, tip1, taxOnFood, taxOnBeverage);
-    double totalBookingBaseCost=hall.calculateBaseCost(costOfBooking, costOfFood, beveragesCost, tip1);
+    double totalBookingBaseCost=hall.calculateBaseCost(costOfBooking, costOfFood, beveragesCost);
+    System.out.println("The cost of hall including booking cost, cost of food and beverages is "+totalBookingBaseCost);
     double  taxApplied=hall.calculateTax(totalBookingBaseCost);
+    System.out.println("The tax applied on base cost is" +taxApplied);
     double  cessTax=hall.calculateCess(numguests, totalBookingBaseCost);
+    System.out.println("The cess Tax  on"+numguests+" guests is"+cessTax);
     double totalCost=hall.calculateTheTotalCost(totalBookingBaseCost, taxApplied, cessTax);
     System.out.println("The total cost of the Banquet hall is"+totalCost);
+	}else {
+	 System.out.println("Pleasefill the right amount");
 	}
+}
 }
